a825431d410cf937ff32c6736df6631f3b2556b3
jgabrielcode <158171407+jgabrielcode@users.noreply.github.com>
2025-05-25 23:25:14 -0300

    add
    New design

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const frm = document.querySelector("form")
+const out = document.querySelector("#out")
+const container = document.querySelector(".container")
+const login = document.querySelector(".login")
+const iEmail = frm.querySelector("#iEmail")
+const btMenu = document.querySelector("#bt-menu")
+
+frm.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    out.innerText = `Obrigado por se cadastrar! Fique de olho no seu e-mail e 
+    prepare-se para elevar sua experiência digital!
+`
+    
+    
+})
+
+btMenu.addEventListener("click", ()=>{
+container.className = "sumir"
+login.style.display = "block"
+})
