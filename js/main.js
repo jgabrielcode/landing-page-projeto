14383b27607bc5286768f3adef82391f2b5fd3cd
jgabrielcode <158171407+jgabrielcode@users.noreply.github.com>
2025-06-01 21:19:08 -0300

    add
    Validacao de email

--- js/main.js
+++ js/main.js
@@ -5,11 +5,19 @@ const login = document.querySelector(".login")
 const iEmail = frm.querySelector("#iEmail")
 const btMenu = document.querySelector("#bt-menu")
 
+const emails = []
+
 frm.addEventListener("submit", (e)=>{
     e.preventDefault()
-    out.innerText = `Obrigado por se cadastrar! Fique de olho no seu e-mail e 
-    prepare-se para elevar sua experiência digital!
-`
+
+    const email = iEmail.value.toLowerCase().trim()
+
+        emails.push(email)
+        out.innerText = `Obrigado por se cadastrar! Fique de olho no seu e-mail e 
+        prepare-se para elevar sua experiência digital!`
+        out.style.color = "green"
+ 
+        console.log(emails)
 })
 
 btMenu.addEventListener("click", ()=>{
