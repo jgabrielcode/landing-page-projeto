0bc4ef4eb26622ca04f05c9bcd92338907e4743f
jgabrielcode <158171407+jgabrielcode@users.noreply.github.com>
2025-06-03 11:30:53 -0300

    add

--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
 const frm = document.querySelector("form")
 const out = document.querySelector("#out")
-const container = document.querySelector(".container")
-const login = document.querySelector(".login")
 const iEmail = frm.querySelector("#iEmail")
-const btMenu = document.querySelector("#bt-menu")
+
+const emails = []
 
 frm.addEventListener("submit", (e)=>{
     e.preventDefault()
+    emails.push(iEmail.value)
     out.innerText = `Obrigado por se cadastrar! Fique de olho no seu e-mail e 
-    prepare-se para elevar sua experiência digital!
-`
-})
-
-btMenu.addEventListener("click", ()=>{
-container.className = "sumir"
-login.className = "exibe"
+    prepare-se para elevar sua experiência digital!`
+    
 })
